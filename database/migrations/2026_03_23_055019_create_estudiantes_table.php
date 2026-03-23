dd5c697dc1fde56cc15cc81a1e9a7b324e1b9628
Bladimir <bladiorte551@gmail.com>
2026-03-22 23:56:10 -0600

    Migraciones

---
+++ database/migrations/2026_03_23_055019_create_estudiantes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('estudiantes', function (Blueprint $table) {
+            $table->id();
+            $table->String('nombre');
+            $table->String('apellidoPaterno');
+            $table->String('apellidoMaterno');
+            $table->String('correo');
+            $table->String('matricula');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('estudiantes');
+    }
+};
