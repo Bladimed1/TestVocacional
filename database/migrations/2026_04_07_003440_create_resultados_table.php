3e9eaab7450f8dabc0a33a8617842d6b78e6ce1b
Bladimir <bladiorte551@gmail.com>
2026-04-06 18:55:12 -0600

    migraciones

---
+++ database/migrations/2026_04_07_003440_create_resultados_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resultados', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_estudiante'); 
+            $table->foreign('id_estudiante')->references('id')->on('estudiantes')->onDelete('cascade');
+
+            // Especialidad recomendada (resultado del test)
+            $table->unsignedBigInteger('id_especialidad'); 
+            $table->foreign('id_especialidad')->references('id')->on('especialidades')->onDelete('cascade');
+
+            // Puntaje obtenido en el test (opcional pero muy útil)
+            $table->integer('puntaje')->nullable();
+
+            // Fecha en que se hizo el test (Laravel ya guarda created_at, pero esto es más claro)
+            $table->timestamp('fecha_aplicacion')->useCurrent();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resultados');
+    }
+};
