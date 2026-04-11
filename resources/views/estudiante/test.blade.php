b9c01b2df4ff5eb1bdc3bdb95dd58168f738631f
Bladimir <bladiorte551@gmail.com>
2026-04-11 01:07:48 -0600

    preguntas test

--- resources/views/estudiante/test.blade.php
+++ resources/views/estudiante/test.blade.php
@@ -6,6 +6,25 @@
     <title>Document</title>
 </head>
 <body>
-    
+
+    @foreach ($categoriaEscala as $categoria)
+        <h4>{{$categoria['nombre']}}</h4>
+    @foreach ($categoria['preguntas'] as $pregunta)
+      <label for="pregunta">{{$pregunta['texto']}}</label> <br>
+      <input type="radio" name="{{$pregunta['especialidad']}}" id=""> 1 <br>
+      <input type="radio" name="{{$pregunta['especialidad']}}" id=""> 2 <br>
+      <input type="radio" name="{{$pregunta['especialidad']}}" id=""> 3 <br>
+      <input type="radio" name="{{$pregunta['especialidad']}}" id=""> 4 <br>
+      <input type="radio" name="{{$pregunta['especialidad']}}" id=""> 5 <br>
+    @endforeach
+    @endforeach
+
+        <h4>{{$categoriaFinal['nombre']}}</h4>
+    @foreach ($categoriaFinal['preguntas'] as $pregunta)
+
+     <label for="pregunta">{{$pregunta['texto']}}</label> <br>
+   
+    @endforeach
+
 </body>
 </html>
